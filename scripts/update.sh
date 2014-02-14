6caa5e3ad12b1870e6cf69542918aa4d1deffb1f
Freek1A <Freek1A@users.noreply.github.com>
2014-02-14 23:14:09 +0100

    Fixed spacing.

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,10 +8,10 @@ fi
 md5sumnew=`md5sum spigotnew.jar | cut -b-32`
 md5sumold=`md5sum spigot.jar | cut -b-32`
 if [ "$md5sumnew" = "$md5sumold" ]; then
-echo "Spigot is already up to date."
+  echo "Spigot is already up to date."
   rm spigotnew.jar
 else
-echo "Spigot is not up to date, updating now..."
+  echo "Spigot is not up to date, updating now..."
   ./scripts/backup.sh
   mv spigotnew.jar spigot.jar
   echo "Update finished!"
